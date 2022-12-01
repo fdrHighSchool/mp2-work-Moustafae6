b6cbb58bcaf99a36e106155284cfc880375a159c
Moustafae6 <114923861+Moustafae6@users.noreply.github.com>
2022-12-01 08:18:32 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.utill.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner (System.in);
+    System.out.println("enter a number: ");
+    int num =s 
+ }    
+  public static boolean 
+ for num in range(1,end_num+1):
+  if num %3==0 and num%5==0:
+    System.out.println('fizzbuzz');
+   else if (num%3 ==0):
+    System.out.println('fizz');
+  else if  ( num %5==0:
+  System.out.println('buzz');
+  else:
+   System.out.println(num);
+
+
+for pattern in range(0,2):
+
+  
+  }
+}
